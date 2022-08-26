e92ad6b13beecd1642981118728164ba5f3c9908
ronycastroc <castroroony@gmail.com>
2022-08-26 10:37:22 -0300

    post sign-up

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 
-const server = express();
-server.use(cors());
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+const tweets = [];
+
+app.post('/sign-up', (req, res) => {     
+    const signup = req.body;
+
+    //signup.id = user.length + 1;
+
+    users.push({
+        ...signup,
+        id: users.length + 1
+    });
+
+    res.send('OK');
+});
+
+app.post('/tweets', (req, res) => {
+
+    res.send('OK');
+});
+
+app.get('/tweets', (req, res) => {    
+
+    res.send('OK');
+});
 
 
-
-
-server.listen(5000, () => {
-    console.log('Listen on 5000');
-})
+app.listen(5000, () => console.log('Listen on 5000'));
